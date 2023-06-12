862d34f1f305aca03b602e031c16d22e8def559d
MarcosSouzaa <marcossouza.git@gmail.com>
2023-06-11 21:40:05 -0300

    criado responsividade no projeto, diminuiu a pixelelização da foto

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-//vou incluir esse elemento no titulo, esse elemento represnta a letra
-function ativaLetra(elemento) {
-    const arrTexto = elemento.innerHTML.split('');
-    elemento.innerHTML = ''; //vazio
-    arrTexto.forEach((letra, i) => {
-        setTimeout(() => {
-            elemento.innerHTML += letra;
-        }, 75 * i);
-    });
-}
+function escrevendoLetra() {
+    //vou incluir esse elemento no titulo, esse elemento represnta a letra
+    function ativaLetra(elemento) {/*Tirei a função de global*/
+        const arrTexto = elemento.innerHTML.split('');
+        elemento.innerHTML = ''; //vazio
+        arrTexto.forEach((letra, i) => {
+            setTimeout(() => {
+                elemento.innerHTML += letra;
+            }, 75 * i);
+        });
+    }
 
-const titulo = document.querySelector('.digitando');//meu seletor. Vou criar a função para chamá-lo
-ativaLetra(titulo);
+    const titulo = document.querySelector('.digitando');//meu seletor. Vou criar a função para chamá-lo
+    ativaLetra(titulo);
+}
+escrevendoLetra();
+
+const ativaMenu = document.querySelector('.fa-bars');
+ativaMenu.addEventListener('click', ()=>{
+    ativaMenu.classList.toggle('fa-x') //adiciona a terceira classe no documento e faz um X no click do menu
+})
